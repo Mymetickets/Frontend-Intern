73f9364fa7349236e9c4e4da05c5e49854df123e
Absamis <smallkid1@yahoo.com>
2025-06-28 10:32:59 +0100

    font awesome icon added

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <p class="bg-red-500 text-white p-3">Welcome to tailwind</p>
+    <p class="bg-red-500 text-white p-3">
+        <FontAwesomeIcon icon="fa-solid fa-dog" />
+        Welcome to tailwind
+    </p>
     <fwb-alert type="info">
       Success! You can now use Flowbite Vue in your Vue application 🎉
     </fwb-alert>
   </div>
 </template>
 <script setup>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { FwbAlert } from 'flowbite-vue'
 </script>
 
